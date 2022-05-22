f4ade40e98f3c326b00ee6f247901d14630896c5
zangzelin82 <zangzelin@gmail.com>
2022-05-23 06:27:07 +0800

    syn

--- run.sh
+++ run.sh
@@ -10,60 +10,60 @@
 # python baseline.py --knn_neighbors 10 &
 # python baseline.py --knn_neighbors 11 &
 # python baseline.py --knn_neighbors 12 &
-CUDA_VISIBLE_DEVICES=0 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+CUDA_VISIBLE_DEVICES=0 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 sleep 4s
-CUDA_VISIBLE_DEVICES=1 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+CUDA_VISIBLE_DEVICES=1 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 sleep 4s
-CUDA_VISIBLE_DEVICES=2 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+CUDA_VISIBLE_DEVICES=2 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 sleep 4s
-CUDA_VISIBLE_DEVICES=3 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+CUDA_VISIBLE_DEVICES=3 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 sleep 4s
-CUDA_VISIBLE_DEVICES=4 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+CUDA_VISIBLE_DEVICES=4 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 sleep 4s
-CUDA_VISIBLE_DEVICES=5 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+CUDA_VISIBLE_DEVICES=5 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 sleep 4s
-CUDA_VISIBLE_DEVICES=6 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+CUDA_VISIBLE_DEVICES=6 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 sleep 4s
-CUDA_VISIBLE_DEVICES=7 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+CUDA_VISIBLE_DEVICES=7 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 
 # sleep 4s
-# CUDA_VISIBLE_DEVICES=0 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+# CUDA_VISIBLE_DEVICES=0 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 # sleep 4s
-# CUDA_VISIBLE_DEVICES=1 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+# CUDA_VISIBLE_DEVICES=1 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 # sleep 4s
-# CUDA_VISIBLE_DEVICES=2 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+# CUDA_VISIBLE_DEVICES=2 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 # sleep 4s
-# CUDA_VISIBLE_DEVICES=3 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+# CUDA_VISIBLE_DEVICES=3 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 # sleep 4s
-# CUDA_VISIBLE_DEVICES=4 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+# CUDA_VISIBLE_DEVICES=4 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 # sleep 4s
-# CUDA_VISIBLE_DEVICES=5 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+# CUDA_VISIBLE_DEVICES=5 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 # sleep 4s
-# CUDA_VISIBLE_DEVICES=6 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+# CUDA_VISIBLE_DEVICES=6 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 # sleep 4s
-# CUDA_VISIBLE_DEVICES=7 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+# CUDA_VISIBLE_DEVICES=7 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 # sleep 4s
 
-# CUDA_VISIBLE_DEVICES=0 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
-# CUDA_VISIBLE_DEVICES=1 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
-# CUDA_VISIBLE_DEVICES=2 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
-# CUDA_VISIBLE_DEVICES=3 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
-# CUDA_VISIBLE_DEVICES=4 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
-# CUDA_VISIBLE_DEVICES=5 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
-# CUDA_VISIBLE_DEVICES=6 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
-# CUDA_VISIBLE_DEVICES=7 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+# CUDA_VISIBLE_DEVICES=0 wandb agent cairi/bloodcenter_zzl/3p4843aw &
+# CUDA_VISIBLE_DEVICES=1 wandb agent cairi/bloodcenter_zzl/3p4843aw &
+# CUDA_VISIBLE_DEVICES=2 wandb agent cairi/bloodcenter_zzl/3p4843aw &
+# CUDA_VISIBLE_DEVICES=3 wandb agent cairi/bloodcenter_zzl/3p4843aw &
+# CUDA_VISIBLE_DEVICES=4 wandb agent cairi/bloodcenter_zzl/3p4843aw &
+# CUDA_VISIBLE_DEVICES=5 wandb agent cairi/bloodcenter_zzl/3p4843aw &
+# CUDA_VISIBLE_DEVICES=6 wandb agent cairi/bloodcenter_zzl/3p4843aw &
+# CUDA_VISIBLE_DEVICES=7 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 
-# CUDA_VISIBLE_DEVICES=0 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
-# CUDA_VISIBLE_DEVICES=1 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
-# CUDA_VISIBLE_DEVICES=2 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
-# CUDA_VISIBLE_DEVICES=3 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
-# CUDA_VISIBLE_DEVICES=4 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
-# CUDA_VISIBLE_DEVICES=5 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
-# CUDA_VISIBLE_DEVICES=6 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
-# CUDA_VISIBLE_DEVICES=7 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
+# CUDA_VISIBLE_DEVICES=0 wandb agent cairi/bloodcenter_zzl/3p4843aw &
+# CUDA_VISIBLE_DEVICES=1 wandb agent cairi/bloodcenter_zzl/3p4843aw &
+# CUDA_VISIBLE_DEVICES=2 wandb agent cairi/bloodcenter_zzl/3p4843aw &
+# CUDA_VISIBLE_DEVICES=3 wandb agent cairi/bloodcenter_zzl/3p4843aw &
+# CUDA_VISIBLE_DEVICES=4 wandb agent cairi/bloodcenter_zzl/3p4843aw &
+# CUDA_VISIBLE_DEVICES=5 wandb agent cairi/bloodcenter_zzl/3p4843aw &
+# CUDA_VISIBLE_DEVICES=6 wandb agent cairi/bloodcenter_zzl/3p4843aw &
+# CUDA_VISIBLE_DEVICES=7 wandb agent cairi/bloodcenter_zzl/3p4843aw &
 
-# CUDA_VISIBLE_DEVICES=0 wandb agent cairi/bloodcenter_zzl/66f6xcoy &
-# CUDA_VISIBLE_DEVICES=0 wandb agent cairi/bloodcenter_zzl/66f6xcoy
+# CUDA_VISIBLE_DEVICES=0 wandb agent cairi/bloodcenter_zzl/3p4843aw &
+# CUDA_VISIBLE_DEVICES=0 wandb agent cairi/bloodcenter_zzl/3p4843aw
 
 # echo "Device ID:" $1
 # CUDA_VISIBLE_DEVICES=$1 python main.py --foldindex $[ $1*5+0 ] &&
