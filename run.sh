407909049bc6b3db53222aff9cc4e726a97c3c51
zangzelin_52 <zangzelin@gmail.com>
2022-05-25 16:03:27 +0800

    syn

--- run.sh
+++ run.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 wandb agent cairi/bloodcenter_zzl/6vj9fxdz &
+CUDA_VISIBLE_DEVICES=0 wandb agent cairi/bloodcenter_zzl/7g0unk6s &
 sleep 4s
-CUDA_VISIBLE_DEVICES=1 wandb agent cairi/bloodcenter_zzl/6vj9fxdz &
+CUDA_VISIBLE_DEVICES=1 wandb agent cairi/bloodcenter_zzl/7g0unk6s &
 sleep 4s
-CUDA_VISIBLE_DEVICES=2 wandb agent cairi/bloodcenter_zzl/6vj9fxdz &
+CUDA_VISIBLE_DEVICES=2 wandb agent cairi/bloodcenter_zzl/7g0unk6s &
 sleep 4s
-CUDA_VISIBLE_DEVICES=3 wandb agent cairi/bloodcenter_zzl/6vj9fxdz &
+CUDA_VISIBLE_DEVICES=3 wandb agent cairi/bloodcenter_zzl/7g0unk6s &
 sleep 4s
-CUDA_VISIBLE_DEVICES=4 wandb agent cairi/bloodcenter_zzl/6vj9fxdz &
+CUDA_VISIBLE_DEVICES=4 wandb agent cairi/bloodcenter_zzl/7g0unk6s &
 sleep 4s
-CUDA_VISIBLE_DEVICES=5 wandb agent cairi/bloodcenter_zzl/6vj9fxdz &
+CUDA_VISIBLE_DEVICES=5 wandb agent cairi/bloodcenter_zzl/7g0unk6s &
 sleep 4s
-CUDA_VISIBLE_DEVICES=6 wandb agent cairi/bloodcenter_zzl/6vj9fxdz &
+CUDA_VISIBLE_DEVICES=6 wandb agent cairi/bloodcenter_zzl/7g0unk6s &
 sleep 4s
-CUDA_VISIBLE_DEVICES=7 wandb agent cairi/bloodcenter_zzl/6vj9fxdz &
+CUDA_VISIBLE_DEVICES=7 wandb agent cairi/bloodcenter_zzl/7g0unk6s &
+
+CUDA_VISIBLE_DEVICES=0 wandb agent cairi/bloodcenter_zzl/7g0unk6s &
+sleep 4s
+CUDA_VISIBLE_DEVICES=1 wandb agent cairi/bloodcenter_zzl/7g0unk6s &
+sleep 4s
+CUDA_VISIBLE_DEVICES=2 wandb agent cairi/bloodcenter_zzl/7g0unk6s &
+sleep 4s
+CUDA_VISIBLE_DEVICES=3 wandb agent cairi/bloodcenter_zzl/7g0unk6s &
+sleep 4s
+CUDA_VISIBLE_DEVICES=4 wandb agent cairi/bloodcenter_zzl/7g0unk6s &
+sleep 4s
+CUDA_VISIBLE_DEVICES=5 wandb agent cairi/bloodcenter_zzl/7g0unk6s &
+sleep 4s
+CUDA_VISIBLE_DEVICES=6 wandb agent cairi/bloodcenter_zzl/7g0unk6s &
+sleep 4s
+CUDA_VISIBLE_DEVICES=7 wandb agent cairi/bloodcenter_zzl/7g0unk6s &
